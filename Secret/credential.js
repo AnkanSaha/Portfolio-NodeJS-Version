9bf89995d0ab4d541830cd8d289498fa27368519
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-03 15:25:34 +0530

    Delete Secret directory

--- Secret/credential.js
+++
@@ -1,6 +0,0 @@
-const PORT=5678
-const MOngoDB_URI='mongodb+srv://Ankan157:Ankan1567@serverlessdb.q1ops.mongodb.net/Portfolio?retryWrites=true&w=majority'
-module.exports = {
-    PORT,
-    MOngoDB_URI
-}
