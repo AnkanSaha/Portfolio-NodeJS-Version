3d0522f11d5bc1ee1ce1066e575c8c0735d7df3d
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-03 09:46:12 +0530

    Published January 2023 Version

---
+++ static/admin/js/signin.js
@@ -0,0 +1,66 @@
+document.getElementById("hamburger").addEventListener("click", function () {
+  document.getElementById("navbar-solid-bg").classList.toggle("hidden");
+});
+
+// login page
+// Path: static/admin/js/signin.js
+document.getElementById("submitLogin").addEventListener("click", (e) => {
+  e.preventDefault();
+  let AdminCode = document.getElementById("AdminCode").value;
+  if (AdminCode.length == 0) {
+    alert("Please Enter Admin Code");
+  } else {
+    AdminCodeVerify(AdminCode);
+  }
+});
+
+//auto login
+let Code = localStorage.getItem("AdminCode");
+if (Code == null) {
+  console.log("Admin Code is not set");
+} else if (Code != null) {
+  AdminCodeVerify(Code);
+}
+
+// Admin Code Verify feature
+async function AdminCodeVerify(AdminCode) {
+  try {
+    let response = await fetch("/CodeVerify", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ AdminCode: AdminCode }),
+    });
+    let data = await response.json();
+    if (data.status == "success") {
+      localStorage.setItem("AdminCode", AdminCode);
+      window.location.href = "/admin/dashboard";
+    } else if (data.status == "failed") {
+      alert("Admin Code is incorrect");
+    }
+  } catch (err) {
+    if (err) {
+      console.log(err);
+      alert("Unable to verify Admin Code right now. Please try again later.");
+    }
+  }
+}
+// Gtag Configuration
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-2RK9LW9MGR");
+
+// disable right click in whole page
+document.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+});
+
+// logo animaton
+let logo = document.getElementById("mainlogo");
+logo.classList.toggle("animate-spin");
+setInterval(() => {
+  logo.classList.toggle("animate-spin");
+}, 2000);
