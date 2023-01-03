74cb334f012afcd8e572a9ac7be66805a4985ec0
Ankan Saha <93839804+AnkanSaha@users.noreply.github.com>
2023-01-03 09:40:47 +0530

    Delete Server directory

--- Server/DB_Config.js
+++
@@ -1,6 +0,0 @@
-const URL = `mongodb+srv://Ankan157:Ankan1567@serverlessdb.q1ops.mongodb.net/Portfolio?retryWrites=true&w=majority`
-module.exports = URL
-
-// ..remote url 
-
-//mongodb+srv://Ankan157:Ankan1567@serverlessdb.q1ops.mongodb.net/Portfolio?retryWrites=true&w=majority
