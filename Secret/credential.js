97e9717464c13915ad55b8ea4b5e2cbb6a6a76d9
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-13 00:37:02 +0530

    changed MongoDB Server

--- Secret/credential.js
+++ Secret/credential.js
@@ -1,5 +1,5 @@
 const PORT=5678
-const MOngoDB_URI='mongodb+srv://Ankan157:Ankan1567@serverdb.q1ops.mongodb.net/Portfolio?retryWrites=true&w=majority'
+const MOngoDB_URI='mongodb+srv://Ankan157:Ankan1567@db.q1ops.mongodb.net/Portfolio?retryWrites=true&w=majority'
 module.exports = {
     PORT,
     MOngoDB_URI
