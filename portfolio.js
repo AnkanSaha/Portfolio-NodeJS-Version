83e71e71d6d277477e7027868e6074e083b18ded
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-16 13:48:47 +0530

    Added Cluster Module in This Server

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,6 @@
 const express = require("express"); // express
+const os = require("os"); // os
+const cluster = require("cluster"); // cluster
 const app = express(); // initilize express
 const CORS = require("cors"); // CORS
 const {Router} = require("./router"); // General Routing
@@ -14,21 +16,39 @@ const port = credential.PORT || 5678 // Port
   "https://theankan.live",
 ];
 
-app.use(CORS(Domain)); // setting up CORS
-app.listen(port, () => {
-  console.log(`app listening at port No ${port}`);
-}); // listening port
+// Cluster Configuration
+let cpuCount = os.cpus().length;
 
-app.use(express.urlencoded({ extended: true })); //form data encoding
-app.use(express.json()); //Json Data Encoding
-app.use(express.static("static")); // serving static files
-app.use(Router); // linking File Routing
-app.use(PostRequests); // Linking All Post Requests Routing
-app.use(GetRequests); // Linking All Get Requests Routing
+if (cluster.isMaster) {
+  while (cpuCount>0) {
+    cluster.fork();
+    cpuCount--;
+  }
+   cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+   });
+}
+else{
+  
+  app.use(CORS(Domain)); // setting up CORS
+  app.listen(port, () => {
+    console.log(`app listening at port No ${port}`);
+  }); // listening port
+  
+  app.use(express.urlencoded({ extended: true })); //form data encoding
+  app.use(express.json()); //Json Data Encoding
+  app.use(express.static("static")); // serving static files
+  app.use(Router); // linking File Routing
+  app.use(PostRequests); // Linking All Post Requests Routing
+  app.use(GetRequests); // Linking All Get Requests Routing
+  
+  // pug engine configuration
+  app.set("view engine", "pug");
+  app.set("views", "./static/blogs");
 
-// pug engine configuration
-app.set("view engine", "pug");
-app.set("views", "./static/blogs");
+}
+
 
 // 404 Page
 app.get('*', (request, response) => {
