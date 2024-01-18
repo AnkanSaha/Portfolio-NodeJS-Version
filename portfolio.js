d58d371bc8900f4bcfe5047e18b59e1b59d8ebba
Ankan Saha <ankansahadaluabari@gmail.com>
2024-01-18 10:32:20 +0530

    Added : Added Rate Limiter

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
 const express = require("express"); // express
 const os = require("os"); // os
 const cluster = require("cluster"); // cluster
-const app = express(); // initilize express
+const app = express(); // initialize express
 const CORS = require("cors"); // CORS
 const {Router} = require("./router"); // General Routing
 const PostRequests = require('./Controller/Post Request/Post')
@@ -10,7 +10,16 @@ const credential = require("./Secret/credential.js"); // credential
 const port = credential.PORT || 5678 // Port
 
 // Cors Configuration
-/* Cors Allowed Domains*/  let Domain = '*'; // All Domains
+const CorsConfig = {
+  origin: '*',
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
+  credentials: true,
+  exposedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Access-Control-Allow-Headers', 'Access-Control-Allow-Origin', 'Access-Control-Allow-Methods', 'Access-Control-Allow-Credentials'],
+  maxAge: 86400,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200,
+  preflightContinue: false
+}
 
 // Cluster Configuration
 let cpuCount = os.cpus().length;
@@ -27,7 +36,7 @@ if (cluster.isMaster) {
 }
 else{
   
-  app.use(CORS(Domain)); // setting up CORS
+  app.use(CORS(CorsConfig)); // setting up CORS
   app.listen(port, () => {
     console.log(`app listening at port No ${port}`);
   }); // listening port
