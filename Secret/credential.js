5f2c092dceaed83763360b39a744fc96ed11e10e
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-13 23:45:49 +0530

    Removed: Removed All File for Rewrite the all files

--- Secret/credential.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config() // this is important!
-
-const PORT = process.env.PORT ?? 5000
-const MOngoDB_URI = process.env.MONGODB_URI ?? 'mongodb://localhost:27017/'
-const Database_Name = process.env.DB_NAME ?? 'Portfolio' // this is important!
-module.exports = {
-  PORT,
-  MOngoDB_URI: `${MOngoDB_URI}${Database_Name}`
-}
