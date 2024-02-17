0e6a277dce6597b5cc007478cf4e8b5192cf7778
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-17 21:18:46 +0000

    Removed : Removed All Files for rewrite

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,8 +4,10 @@ sudo apt-get update -y # Update apt-get cache
 sudo apt-get install -y neofetch # Install curl
 
 # Install Dependencies
+cd Client/ # Install Client
+
 npm install --force # Install All Dependencies
-npm run build # Build CSS
+npm run build # Build Client Version
 
 # Setup Docker Containers
 docker-compose up -d # Start Docker Containers in detached mode mention  in docker-compose.yml
