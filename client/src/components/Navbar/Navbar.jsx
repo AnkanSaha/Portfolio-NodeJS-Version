6662f0e891f93577335f37fd864a02eb40fcabd7
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-14 21:54:05 +0530

    Remove unnecessary links from Navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -61,22 +61,6 @@ export default function Navbar () {
               </li>
               <li>
                 <Link
-                  to='/Tech'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  Technology Stack
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/about'
-                  className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-                >
-                  About me
-                </Link>
-              </li>
-              <li>
-                <Link
                   to='/projects'
                   className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
