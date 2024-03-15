34ca3a3aa967a6122dd22f3029cf771a4a64d677
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-15 12:18:45 +0530

    Add new certifications and update UI components

--- client/src/components/Home/Certification.jsx
+++ client/src/components/Home/Certification.jsx
@@ -1,5 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react"; // Import React
+import {
+  Box,
+  Flex,
+  Avatar,
+  Heading,
+  Text,
+  Image,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Button,
+} from "@chakra-ui/react"; // Import Chakra UI components
+import {
+  OwnerPicture,
+  OwnerName,
+  OwnerCertification,
+} from "../../core/Keys/OwnerDetails.keys"; // Import OwnerDetails
 
 // Certification component
 export default function Certification() {
@@ -11,46 +29,63 @@ export default function Certification() {
         </span>{" "}
       </h1>
 
-      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-2 my-5 lg:mx-5">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="/docs/images/blog/image-1.jpg"
-            alt=""
-          />
-        </a>
-        <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Noteworthy technology acquisitions 2021s
-            </h5>
-          </a>
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Read more
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
+      <div className='flex lg:flex-row flex-col flex-wrap'>
+        {OwnerCertification.map((Certification) => {
+          return (
+            <Card
+              key={Certification.CourseName}
+              maxW="md"
+              className="mx-3 my-5 lg:mx-5 w-auto"
             >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
-        </div>
+              <CardHeader>
+                <Flex spacing="4">
+                  <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                    <Avatar
+                      name={Certification.CourseName}
+                      src={Certification.Icons}
+                    />
+
+                    <Box>
+                      <Heading size="sm">{Certification.CourseName}</Heading>
+
+                      <Text>
+                        <strong>Issued By : </strong>
+                        {Certification.Issuer}
+                      </Text>
+                      <Text>
+                        {" "}
+                        <strong> Issue Date: </strong> {Certification.IssueDate}
+                      </Text>
+                    </Box>
+                  </Flex>
+                </Flex>
+              </CardHeader>
+              <CardBody>
+                <Text>{Certification.description}</Text>
+              </CardBody>
+
+              <Button
+                onClick={() => {
+                  window.open(Certification.Details_URL);
+                }}
+                colorScheme="facebook"
+                className="right-5 m-auto"
+              >
+                View Details
+              </Button>
+
+              <CardFooter
+                justify="space-between"
+                flexWrap="wrap"
+                sx={{
+                  "& > button": {
+                    minW: "136px",
+                  },
+                }}
+              ></CardFooter>
+            </Card>
+          );
+        })}
       </div>
     </>
   );
