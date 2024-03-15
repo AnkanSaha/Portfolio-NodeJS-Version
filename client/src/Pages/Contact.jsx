ac3f86bff4b5ca22e06dc95d361aeb07983018bf
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-15 16:07:24 +0530

    Add React Caches and update document title

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -1,17 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react' // Import React
+import React from 'react'; // Import React
 import {React as Service} from 'react-caches'; // Import React Caches
 import { OwnerName } from '../core/Keys/OwnerDetails.keys'; // Import the Owner Name
-
 
 // import Components
 import Navbar from '../components/Navbar/Navbar' // Import the Navbar component
 import Footer from '../components/Footer/Footer' // Import the Footer component
 
 // Main Component
-export default function ProjectsPage () {
+export default function ContactPage () {
   // Change the title of the page
-  Service.UpdateDocumentTitle(`Projects - ${OwnerName}`); // Change the title of the page to "Projects"
+  Service.UpdateDocumentTitle(`Contact - ${OwnerName}`); // Change the title of the page to "Contact"
+  
   return (
     <>
       <Navbar />
--- client/src/core/Router/Router.jsx
+++ client/src/core/Router/Router.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom' // import the React router DOM module
 
 // Import Pages
-import HomePage from '../../Pages/Home' // Import the Home page
-import ProjectsPage from '../../Pages/Projects' // Import the Projects page
+import HomePage from '../../Pages/Home'; // Import the Home page
+import ProjectsPage from '../../Pages/Projects'; // Import the Projects page
+import ContactPage from '../../Pages/Contact'; // Import the Contact page
 
 // Main Router
 const MainRouter = () => {
@@ -11,6 +12,7 @@ const MainRouter = () => {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/projects' element={<ProjectsPage />} />
+        <Route path='/contact' element={<ContactPage />} />
       </Routes>
     </BrowserRouter>
   )
