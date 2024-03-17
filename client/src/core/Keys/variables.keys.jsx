5f13d9166d27f30c7d66a5123b0d546356e02a6b
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-17 16:10:46 +0530

    Add Loading GIF and Skeleton page

--- client/src/core/Keys/variables.keys.jsx
+++ client/src/core/Keys/variables.keys.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react-refresh/only-export-components */
 import { React as Service } from 'react-caches' // Import Service
+import Loading from '../../Assets/Images/loading.gif' // Import Loading GIF
 
 // Export is Development Mode
 export const isDevelopmentMode = import.meta.env.DEV // Global Development Mode
@@ -20,3 +21,6 @@ export const API_Call = new Service.ClassBasedFunctions.API({
     'Content-Type': 'application/json'
   }
 })
+
+// Export the Loading GIF
+export const LoadingGIF = Loading; // Global Loading GIF
