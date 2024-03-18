ed737971b2db0e1153ad37b957b9becd125852ea
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-18 14:17:36 +0000

    Add ContactForm component to ContactPage

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -4,8 +4,9 @@ import { React as Service } from 'react-caches' // Import React Caches
 import { OwnerName } from '../core/Keys/OwnerDetails.keys' // Import the Owner Name
 
 // import Components
-import Navbar from '../components/Navbar/Navbar' // Import the Navbar component
-import Footer from '../components/Footer/Footer' // Import the Footer component
+import Navbar from '../components/Navbar/Navbar'; // Import the Navbar component
+import Footer from '../components/Footer/Footer'; // Import the Footer component
+import ContactForm from '../components/Contact/ContactUs'; // Import the ContactForm component
 
 // Main Component
 export default function ContactPage () {
@@ -15,6 +16,7 @@ export default function ContactPage () {
   return (
     <>
       <Navbar />
+      <ContactForm />
       <Footer />
     </>
   )
---
+++ client/src/components/Contact/ContactUs.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from "react"; // Import React
+
+// Import Component
+import MainText from '../Header/HeaderText'; // Main Text
+
+// Main Component
+export default function ContactForm() {
+  return (
+    <>
+      <MainText Text='Contact US' />
+    </>
+  );
+}
