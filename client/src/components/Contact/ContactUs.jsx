87f0a9c05b2f01d43aa8d0ebcf47d2ee6767e905
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-18 14:53:21 +0000

    Update ContactUs component UI

--- client/src/components/Contact/ContactUs.jsx
+++ client/src/components/Contact/ContactUs.jsx
@@ -12,18 +12,25 @@ export default function ContactForm() {
     <>
       <MainText Text="Contact US" />
       <div className="card w-80 bg-base-100 shadow-xl my-5 ml-[2rem] mr-[2.25rem] lg:w-96 lg:ml-[36rem]">
-        <figure className="px-10 pt-10">
-          <img
-            src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-            alt="Shoes"
-            className="rounded-xl"
-          />
+        <figure className="px-10 lg:pt-10 pt-[6.5rem] flex flex-col lg:space-y-9 space-y-11">
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input type="text" className="grow" placeholder="Email" />
+          </label>
+          <input type="text" placeholder="Enter Title for this Request" className="input input-bordered input-accent w-full max-w-xs" />
+          <input type="text" placeholder="Describe all things here..." className="input input-bordered input-accent w-full max-w-xs" />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">Shoes!</h2>
-          <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary">Send Now</button>
           </div>
         </div>
       </div>
