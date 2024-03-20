8aa444d5b52906d269b71642bf231340b3fa235e
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-20 17:32:39 +0000

    Refactor MainLoginBox component to include password state and onchange handler

--- client/src/components/Admin/Login/MainLoginBox.jsx
+++ client/src/components/Admin/Login/MainLoginBox.jsx
@@ -1,14 +1,35 @@
 import React from "react"; // eslint-disable-line no-unused-vars
-import { FaLock } from "react-icons/fa"; // Import from React
+import { FaLock } from "react-icons/fa"; // Import Icon
+
 // import ChakraUI
 import { Input, Button } from "@chakra-ui/react"; // ChakraUI Input
 
 // Main component
 export default function LoginBox() {
+  // States
+  const [Password, setPassword] = React.useState({
+    ADMIN_PASSWORD: "",
+  });
+
+  // Onchange handlers
+  const onchange = (event) => {
+    console.log(Password);
+    event.preventDefault(); // prevent default
+    setPassword({
+      ...Password,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <>
       <div className="mx-10 my-52 lg:my-44 space-y-8">
-        <Input placeholder="Enter Admin Password" />
+        <Input
+          placeholder="Enter Admin Password"
+          onChange={onchange}
+          value={Password.ADMIN_PASSWORD}
+          name="ADMIN_PASSWORD"
+        />
         <Button
           rightIcon={<FaLock />}
           leftIcon={<FaLock />}
