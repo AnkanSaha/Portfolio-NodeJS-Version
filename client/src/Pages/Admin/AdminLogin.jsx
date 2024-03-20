567165fcbe8671ea9731fdcfd71d11373bf49633
Ankan Saha <ankansahadaluabari@gmail.com>
2024-03-20 15:02:04 +0000

    Refactor AdminLogin component to include Navbar and Footer components

--- client/src/Pages/Admin/AdminLogin.jsx
+++ client/src/Pages/Admin/AdminLogin.jsx
@@ -1,12 +1,16 @@
-import React from 'react'; // eslint-disable-line no-unused-vars
+import React from "react"; // eslint-disable-line no-unused-vars
 
+// Import Component
+import Navbar from "../../components/Navbar/Navbar"; // Navbar
+import Footer from "../../components/Footer/Footer"; // Footer
 
 // Main Page Component
-
 export default function AdminLogin() {
   return (
     <>
-    <h1>Hello</h1>
+      <Navbar />
+      <h1>Hello</h1>
+      <Footer />
     </>
-  )
+  );
 }
