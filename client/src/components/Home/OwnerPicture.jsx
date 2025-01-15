860c0b5761ab895486a35d738a821c3c73c84432
Ankan Saha <ankansahadaluabari@gmail.com>
2025-01-15 22:29:00 +0530

    Add rounded styling to OwnerPicture component

--- client/src/components/Home/OwnerPicture.jsx
+++ client/src/components/Home/OwnerPicture.jsx
@@ -3,12 +3,12 @@ import React from 'react' // Import React
 import { OwnerPicture, OwnerName } from '../../core/Keys/OwnerDetails.keys' // Import the variables from the variables.keys file
 
 // Main Component
-export default function OwnerPictureShower () {
+export default function OwnerPictureShower() {
   return (
     <>
       <img
         src={OwnerPicture}
-        className='ml-[4rem] mt-11 lg:w-[50%] w-[60%]'
+        className='ml-[4rem] mt-11 lg:w-[50%] w-[60%] rounded-full'
         alt={`${OwnerName}'s Image`}
       />
     </>
